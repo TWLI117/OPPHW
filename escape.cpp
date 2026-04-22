a3bc55fd6460649e93d2307151d6f456ed46b922
TWLI117 <s1140531@dmhs.kh.edu.tw>
2026-04-22 15:22:14 +0800

    Add escape.cpp to print messages with characters
    Print multiple lines with Chinese characters and escape sequences.

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    cout << "1快樂星球" << endl;
+    cout << "2快樂 星球" << endl;
+    cout << "3快樂/t星球" << endl;
+    cout << "4快樂/n星球" << endl;
+    cout << "5//S快樂星球//" << endl;
+    cout << "6/快樂星球/;" << endl;
+    return 0;
+}
